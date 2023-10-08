24db19078bd233cdcaf50a60014e10783294b4dd
Lois <loisobrero@gmail.com>
2023-10-08 16:20:57 -0500

    adding html for onboarding

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+// JavaScript to handle language selection
+document.getElementById("language-select").addEventListener("change", function() {
+    const selectedLanguage = this.value;
+    alert(`You selected ${selectedLanguage} as your preferred language.`);
+});
